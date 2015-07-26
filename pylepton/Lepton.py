cc5019b85ed2f6f343f5575dd75cda14b46b207e
Jacek Burys <jacekburys@gmail.com>
2015-07-26 00:45:55 -0700

    changed speed, lastTemp

--- pylepton/Lepton.py
+++ pylepton/Lepton.py
@@ -42,7 +42,8 @@ class Lepton(object):
   VOSPI_FRAME_SIZE_BYTES = VOSPI_FRAME_SIZE * 2
   MODE = SPI_MODE_3
   BITS = 8
-  SPEED = 18000000
+  #SPEED = 18000000
+  SPEED = 8000000
   SPIDEV_MESSAGE_LIMIT = 24
 
   def __init__(self, spi_dev = "/dev/spidev0.0"):
